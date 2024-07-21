eb61f01a898fb4c9071a10669b2f8b37c68f7a4b
Elena <_elenochka_@mail.ru>
2024-07-21 23:35:03 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name='Elena'
+print('Name: ',name)
+age=44
+print('Age: ',age)
+age=age+1
+print('New Age: ',age)
+is_student=True
+print('Is_student: ',is_student)
---
+++ variable.py
@@ -0,0 +1,4 @@
+comple_works=12
+time_spent=1.5
+kurs='Python'
+print('Курс: ',kurs,', всего задач: ',comple_works,', затрачено часов: ',time_spent,', среднее время выполнения ',(time_spent)/(comple_works),' часа')
