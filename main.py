d4014538e876aa2de254be59764d556e2a5c8733
E-Iena <_elenochka_@mail.ru>
2024-07-21 17:13:28 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+print('Hello wourd')
+print('1st program')
+print((9**0.5)*5)
+print('2st program')
+print(9.99>9.98 and 1000!=1000.1)
+print('3st program')
+one=1234
+two=5678
+print(int(str(one)[1:3])+int(str(two)[1:3]))
+print('Denis')
+print((one)//10%100+(two)//10%100)
+print('4st program')
+three=13.42
+four=42.13
+print((three)//1==(four)*100%100 or (four)//1==(three)*100%100)
