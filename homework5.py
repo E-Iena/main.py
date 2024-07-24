36de7da6fb7e4ed139459c021718a1eab5913215
Elena <_elenochka_@mail.ru>
2024-07-24 21:58:45 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,8 @@
+immutable_var=1, 2,'one', True
+print(immutable_var)
+#immutable_var.append(False)
+#print(immutable_var)
+mutable_list=[1, 2,'one',True]
+mutable_list.append([15,'w'])
+mutable_list[1]='f'
+print(mutable_list)
