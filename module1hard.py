c1b0552303a6b2eb00e6cb1c41bf583c4bb8585f
Elena <_elenochka_@mail.ru>
2024-07-26 23:15:57 +0300

    Add files via upload

--- module1hard.py
+++ module1hard.py
@@ -4,9 +4,9 @@ students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 print('Оценок ', len(grades), ' штук')
 grades = [sum(grades[0]) / len(grades[0]),
           sum(grades[1]) / len(grades[1]),
-          sum(grades[2]) // len(grades[2]),
-          sum(grades[3]) // len(grades[3]),
-          sum(grades[4]) // len(grades[4])]
+          sum(grades[2]) / len(grades[2]),
+          sum(grades[3]) / len(grades[3]),
+          sum(grades[4]) / len(grades[4])]
 print('Список средних баллов: ', grades)
 #  с именами
 print('Имен - ', len(students))
