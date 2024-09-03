9cd134b6e16a601e32e38219639505781d9e2fb0
Elena <_elenochka_@mail.ru>
2024-09-03 23:30:16 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,27 +1,35 @@
-def send_email(sender='university.help@gmail.com',recipient=[],message=[]):
-    if sender==recipient:
+def send_email(sender='university.help@gmail.com', recipient=[], message=[]):
+    if sender == recipient:
         print('Нельзя отправить письмо самому себе!')
     else:
-        y='True'
-        for i in range(1,(len(recipient)-3)):
-            if recipient[i]=='@':
-                y='True'
-                if recipient[-4:]=='.com' or recipient[-3:]=='.ru' or recipient[-4:]=='.net':
-                    if sender=='university.help@gmail.com':
-                        print('Письмо успешно отправлено с адреса ',sender,' на адрес ',recipient)
+        y = True
+        for i in range(1, (len(recipient) - 3)):
+            if recipient[i] == '@':
+                y = True
+                if recipient[-4:] == '.com' or recipient[-3:] == '.ru' or recipient[-4:] == '.net':
+                    if sender == 'university.help@gmail.com':
+                        print('Письмо успешно отправлено с адреса ', sender, ' на адрес ', recipient)
+                        break
                     else:
-                        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса ',sender,' на адрес ',recipient)
+                        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса ', sender, ' на адрес ', recipient)
+                        break
                 else:
-                    print('Невозможно отправить письмо с адреса ',sender,' на адрес ',recipient)
+                    print('Невозможно отправить письмо с адреса ', sender, ' на адрес ', recipient)
+                    break
             else:
-                y='False'
+                y = False
                 continue
-        if y==False:
-            print('2 Невозможно отправить письмо с адреса ',sender,' на адрес ',recipient)
-            
-send_email(recipient='university.help@gmail.com',message='письмо текст')
-send_email(recipient='univ@mail.net',message='письмо текст')
-send_email(recipient='univ@mail.ya',message='письмо текст')
-send_email(sender='university@gmail.com',recipient='adres@gmail.ru',message='письмо текст')
+        if y == False:
+            print('Нет знака @. Невозможно отправить письмо с адреса ', sender, ' на адрес ', recipient)
 
-send_email(recipient='unmail.com',message='письмо текст')
+
+print('1 вариант')
+send_email(recipient='university.help@gmail.com', message='текст письма')
+print('2 вариант')
+send_email(recipient='univ@mail.net', message='текст письма')
+print('3 вариант')
+send_email(recipient='univ@mail.ya', message='текст письма')
+print('4 вариант')
+send_email(sender='university@gmail.com', recipient='adres@gmail.ru', message='текст письма')
+print('5 вариант')
+send_email(recipient='un_mail.com', message='текст письма')
