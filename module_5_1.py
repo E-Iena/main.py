11e4da8954c5a1f76f29d1322fc072e710594d0e
Elena <_elenochka_@mail.ru>
2024-09-14 20:30:39 +0300

    Add files via upload

--- module_5_1.py
+++ module_5_1.py
@@ -1,15 +1,16 @@
-class House() :
+class House:
 
 
 def __init__(self, name, number_of_floors):
     self. name=name
-    self. number_of_floors=number_of_floors
+    self.number_of_floors=number_of_floors ) 
+    self. new_floor
     
     
-def go_to(new_floor):
+def go_to(self):
     if self. new_floor<=self.number_of_floors_of_floors:
-        for 1 in len(self.new_floor:
-        print(f' этаж ',{i})
+        for 1 in len(self.new_floor-1):
+            print(f' этаж ',{i})
     else:
         print('Такого этажа не существует')
         
---
+++ module_5_12.py
@@ -0,0 +1,42 @@
+class House :
+
+
+def __init__(self, name, number_of_floors):
+    self. name=name
+    self. number_of_floors=number_of_floors
+    self. go_to()
+    
+def go_to(self):
+    if self. new_floor<=self.number_of_floors_of_floors:
+        for 1 in len(self.new_floor:
+        print(f' этаж '{i})
+    else:
+        print('Такого этажа не существует')
+        
+
+def len(self):
+    return self. number_of_floors
+
+
+def str(self):
+    print(f' Название: {self.name}, количество этажей: {self.number_of_floors} )
+
+
+h1=House ('ЖК Горский',30)
+h2=House('Домик в деревне', 2)
+h1.go_to(5)
+h2. go_to(10)
+
+
+h3=House(' ЖК Эльбрус', 10)
+h4=House('ЖК Акация', 20)
+str
+print(h1)
+print(h2)
+
+len
+print(len(h1))
+print(len(h2))
+
+    
+    
